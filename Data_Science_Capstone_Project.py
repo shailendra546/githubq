46044c03548c1c1eb7e2f95cef1de9eae69f550d
walkman <walkman@example.com>
2020-06-27 05:07:16 +0530

    New ipython Notebook

---
+++ Data_Science_Capstone_Project.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# We will use this notebook for the capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print(' Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
